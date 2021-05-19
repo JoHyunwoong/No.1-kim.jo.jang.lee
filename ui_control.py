8812d2a08210f5d6d24ce80b266dbf9e9ce910c4
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2021-05-19 21:55:39 +0900

    비동기 구현

--- ui_control.py
+++ ui_control.py
@@ -1,20 +1,22 @@
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtCore import QPropertyAnimation
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication
+from PyQt5.QtCore import QPropertyAnimation, QThread, pyqtSignal
 from ui_version1 import Ui_MainWindow
 import sys
 import time
-from threading import Thread
+from multiprocessing import Process, Queue
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
+    def __init__(self, q):
+        super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.consumer = Consumer(q)
+        self.consumer.poped.connect(self.show_popup)
+        self.consumer.start()
         self.first_percent = 30         # 초기값 설정
         self.second_percent = 70
-
+        self.pump_error_value = 0
         self.ui.Pages_Widget.setCurrentWidget(self.ui.home_page)
 
         self.ui.Btn_Toggle.clicked.connect(lambda: self.toggleMenu(200, True))
@@ -55,19 +57,24 @@ class MainWindow(QMainWindow):
 
 # 스타트기능
     def makestart(self):        # 제조시작
-        self.thread1 = Thread(target=self.make_process)
-        self.thread1.start()
+        self.ui.start_btn.setEnabled(False)     # 중복실행을 위해 버튼 비활성화
+        p = Process(name="pump_worker", target=pump_worker, args=(q,))
+        p.start()
+
 
     def make_process(self):     # 제조과정
-        self.ui.start_btn.setEnabled(False)     # 중복실행을 위해 버튼 비활성화
-        '''return_value = function()             # 함수 실행
-        if return_value == 1:
-            display error  popup
+
+        '''return_value = function() '''            # 함수 실행
+        '''
         else if return_value == 2:
             display amount shortage popup
         else if return_value == 0:
             speaker '''
-        self.ui.start_btn.setEnabled(True)
+        time.sleep(1)
+
+        self.pump_error_value = 2
+        if self.pump_error_value == 2:
+            self.show_popup()
 
 # ui기능
     def toggleMenu(self, maxWidth, enable):         # 토글기능구현
@@ -88,8 +95,44 @@ class MainWindow(QMainWindow):
             self.animation.setEndValue(widthExtended)
             self.animation.start()
 
+    def show_popup(self, data):
+        if data == str(2):
+            msg = QMessageBox()         # pump error
+            x = msg.exec_()
+        elif data == str(1):            # shortage
+            msg = QMessageBox()
+            x = msg.exec_()
+        elif data == str(0):
+            speaker_worker()
+        self.ui.start_btn.setEnabled(True)
+
+# pump process
+def pump_worker(q):
+    data = str(2)
+    #data = str(pump())         # data = return value of pump function
+    q.put(data)
+
+#def speaker_worker():
+    # speaker
+
+# read q and connect thread
+class Consumer(QThread):
+    poped = pyqtSignal(str)
+
+    def __init__(self, q):
+        super().__init__()
+        self.q = q
+
+    def run(self):
+        while True:
+            if not self.q.empty():
+                data = q.get()
+                self.poped.emit(data)
+
 # 실행
 if __name__ == '__main__':
+    q = Queue()
+
     app = QApplication(sys.argv)
-    main_win = MainWindow()
-    sys.exit(app.exec_())
+    main_win = MainWindow(q)
+    app.exec_()
