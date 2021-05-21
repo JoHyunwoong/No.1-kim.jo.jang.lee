67b362a7319831963af57f407deda9827d82d499
hyobin96 <81142510+hyobin96@users.noreply.github.com>
2021-05-22 00:40:13 +0900

    패치1
    펌프코드 전역변수 함수내부로 내재화
    ui monitor 페이지에 온도, 음료 잔량 확인가능, 음료 교체버튼 추가
    ui setting 페이지에 온도설정 디자인 추가, 기능은 미구현

--- ui_control2.py
+++ ui_control2.py
@@ -1,10 +1,10 @@
 from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication
 from PyQt5.QtCore import QPropertyAnimation, QThread, pyqtSignal
-from ui_version1 import Ui_MainWindow
+from ui_version2 import Ui_MainWindow
 import sys
-import time
 from queue import Queue
 from threading import Thread
+from pump import *
 
 class MainWindow(QMainWindow):
     def __init__(self, q):
@@ -17,7 +17,12 @@ class MainWindow(QMainWindow):
         self.consumer.start()
         self.first_percent = 30         # 초기값 설정
         self.second_percent = 70
-        self.pump_error_value = 0
+        self.amount_sso = 360
+        self.amount_mac = 500
+        self.ui.display_first_percent.display(str(self.first_percent))
+        self.ui.display_second_percent.display(str(self.second_percent))
+        self.ui.display_remains1.display(str(self.amount_sso))
+        self.ui.display_remains2.display(str(self.amount_mac))
         self.ui.Pages_Widget.setCurrentWidget(self.ui.home_page)
 
         self.ui.Btn_Toggle.clicked.connect(lambda: self.toggleMenu(200, True))
@@ -29,9 +34,11 @@ class MainWindow(QMainWindow):
         self.ui.up2_btn.clicked.connect(self.up2)
         self.ui.start_btn.clicked.connect(self.makestart)
 
+        self.ui.replace_btn1.clicked.connect(self.replace_sso)
+        self.ui.replace_btn2.clicked.connect(self.replace_mac)
         self.show()
 
-# 페이지 기능
+    # 페이지 기능
     def showhomepage(self):
         self.ui.Pages_Widget.setCurrentWidget(self.ui.home_page)
 
@@ -41,7 +48,7 @@ class MainWindow(QMainWindow):
     def showsetting(self):
         self.ui.Pages_Widget.setCurrentWidget(self.ui.setting_page)
 
-# 농도 업다운 기능
+    # 농도 업다운 기능
     def up1(self):
         if self.first_percent != 100:
             self.first_percent += 10
@@ -56,14 +63,14 @@ class MainWindow(QMainWindow):
         self.ui.display_first_percent.display(str(self.first_percent))
         self.ui.display_second_percent.display(str(self.second_percent))
 
-# 스타트기능
+    # 스타트기능
     def makestart(self):        # 제조시작
         self.ui.start_btn.setEnabled(False)     # 중복실행을 위해 버튼 비활성화
-        p1 = Thread(target=pump_worker, args=(self.q,))
+        p1 = Thread(target=self.worker, args=(self.q,))
         p1.start()
 
 
-# ui기능
+    # ui기능
     def toggleMenu(self, maxWidth, enable):         # 토글기능구현
         if enable:
 
@@ -82,27 +89,43 @@ class MainWindow(QMainWindow):
             self.animation.setEndValue(widthExtended)
             self.animation.start()
 
+    # 오류 팝업
     def show_popup(self, data):
-        if data == str(2):
+        if data == str(1):
             msg = QMessageBox()         # pump error
+            msg.setWindowTitle("오류 발생")
+            msg.setText("펌프에서 문제가 발생했습니다.")
             x = msg.exec_()
-        elif data == str(1):            # shortage
+        elif data == str(2):            # shortage
             msg = QMessageBox()
+            msg.setWindowTitle("음료 부족")
+            msg.setText("음료가 부족합니다.")
             x = msg.exec_()
         elif data == str(0):
-            p2 = Process(name="speaker", target=speaker_worker)
-            p2.start()
+            self.ui.display_remains1.display(str(self.amount_sso))
+            self.ui.display_remains2.display(str(self.amount_mac))
         self.ui.start_btn.setEnabled(True)
 
-# pump process
-def pump_worker(q):
-    data = str(2)
-    #data = str(pump())         # data = return value of pump function
-    q.put(data)
-    
+    # 음료 리필
+    def replace_sso(self):
+        self.amount_sso = 360
+        self.ui.display_remains1.display(str(self.amount_sso))
+
+    def replace_mac(self):
+        self.amount_mac = 500
+        self.ui.display_remains2.display(str(self.amount_mac))
+
+    # pump worker
+    def worker(self, q):
+        pump_error_value, self.amount_sso, self.amount_mac = pumpAlcohol(self.first_percent/100, self.amount_sso, self.amount_mac)
+        print(pump_error_value, self.amount_sso, self.amount_mac) # test print
+        q.put(str(pump_error_value))
+        #if pump_error_value == 0:
+            #buzzer()
+
 # speaker process
-#def speaker_worker():
-    # speaker
+#def buzzer():
+    # buzzer
 
 # read q and connect thread
 class Consumer(QThread):
@@ -120,7 +143,6 @@ class Consumer(QThread):
 
 def main():
     q = Queue()
-
     app = QApplication(sys.argv)
     main_win = MainWindow(q)
     sys.exit(app.exec_())
