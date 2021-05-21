1cf7a7bdfd9fe601fd0494e2cf7bfc57f7b9d9c3
JoHyunwoong <81168001+JoHyunwoong@users.noreply.github.com>
2021-05-21 23:48:14 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,17 @@
+'''
+사전 순서: 1. 명령창에 sudo modprobe w1-gpio && sudo modprode w1_therm 
+          2. 명령창에 sudo nano /boot/config.text (config.text 파일 열기 위함)
+          3. config.text 파일 마지막에 dtoverlay = w1-gpio 추가
+          4. 명령창에 ls -l /sys/bus/w1/devices/ 친 후 장치 주소 확인(28-00000xxxxx)
+'''
+      
+
 import time
 
 
 def displayTemp():
 
-    tempFile = open("/sys/bus/w1/devices/(센서 주소)/w1_slave")
+    tempFile = open("/sys/bus/w1/devices/장치 주소(사전 순서 3)/w1_slave")
     theText = tempFile.read()
     tempFile.close()
     tempDate = theText.split("\n")[1].split(" ")[9]
