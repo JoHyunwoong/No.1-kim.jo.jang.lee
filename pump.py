5ee2fbca2fa22158b87dbd1df3e6bb58663f5bce
JoHyunwoong <81168001+JoHyunwoong@users.noreply.github.com>
2021-06-07 14:09:36 +0900

    Update pump.py
    초기 상태값 isFirst 변수 추가

--- pump.py
+++ pump.py
@@ -2,12 +2,14 @@ import RPi.GPIO as GPIO
 import time
 
 
-def pumpAlcohol(rate, amount_sso, amount_mac):
+def pumpAlcohol(rate, amount_sso, amount_mac, isFirst):
 
     speed = 100  #pump speed(pwm)
     sec1 = 0   #total sec of sso
     sec2 = 0   #total sec of mac
-    amount_per_sec = 5 # output of pump per second
+    amount_per_sec = 28 # output of pump per second
+    global amount_sso
+    global amount_mac
 
     if (amount_sso > 360 * 0.05) and (amount_mac > 500 * 0.05) :
 
@@ -15,20 +17,24 @@ def pumpAlcohol(rate, amount_sso, amount_mac):
             # initialize GPIO
             GPIO.setmode(GPIO.BCM)
 
-            GPIO.setup(23, GPIO.OUT)
-            GPIO.output(23, False)
+            GPIO.setup(12, GPIO.OUT)
+            GPIO.output(12, False)
 
-            GPIO.setup(24, GPIO.OUT)
-            GPIO.output(24, False)
+            GPIO.setup(13, GPIO.OUT)
+            GPIO.output(13, False)
 
-            my_pwm = GPIO.PWM(23, 300)
-            my_pwm = GPIO.PWM(24, 300)
+            my_pwm = GPIO.PWM(12, 300)
+            my_pwm = GPIO.PWM(13, 300)
             my_pwm.start(0)
 
             my_pwm.ChangeDutyCycle(speed) #start pwm
-
-            sec1 = (180 * rate) / amount_per_sec
-            sec2 = (180 * (1 - rate)) / amount_per_sec
+            
+            if isFirst == 1:
+              sec1 = (180 * rate) / amount_per_sec + 1
+              sec2 = (180 * (1 - rate)) / amount_per_sec + 1
+            else:
+              sec1 = (180 * rate) / amount_per_sec 
+              sec2 = (180 * (1 - rate)) / amount_per_sec 
             
             amount_sso -= sec1 * amount_per_sec
             amount_mac -= sec2 * amount_per_sec
@@ -52,14 +58,14 @@ def pumpAlcohol(rate, amount_sso, amount_mac):
                 return 6, amount_sso, amount_mac  # display 'amount shortage' in UI
 
             # pump output
-            GPIO.output(23, True)
-            GPIO.output(24, True)
+            GPIO.output(12, True)
+            GPIO.output(13, True)
             time.sleep(sec1)
 
-            GPIO.output(23, False)
+            GPIO.output(12, False)
 
             time.sleep(sec2 - sec1)
-            GPIO.output(24, False)
+            GPIO.output(13, False)
 
             GPIO.cleanup()
             
