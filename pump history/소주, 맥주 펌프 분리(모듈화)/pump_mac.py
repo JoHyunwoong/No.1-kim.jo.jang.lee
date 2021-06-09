3b3a30128cfb173b3cd520bbb72f918b993c5886
JoHyunwoong <81168001+JoHyunwoong@users.noreply.github.com>
2021-06-09 23:49:19 +0900

    Update pump_mac.py
    남은 양 계산하는 부분 추가

--- pump history/소주, 맥주 펌프 분리(모듈화)/pump_mac.py
+++ pump history/소주, 맥주 펌프 분리(모듈화)/pump_mac.py
@@ -21,13 +21,16 @@ def pumpMac(rate, isFirst2, isReplay2, amount_mac, mac_2nd, amount_per_sec_mac):
         if isFirst2 == 1:
                 sec = (180 * (1 - rate)) / amount_per_sec_mac + 1.3
                 isFirst2 = 0
+                amount_mac -= (180 * (1 - rate))
         else:
             if amount_mac > (180 * (1 - rate)) * (1 + 0.05):
                 sec = (180 * (1 - rate)) / amount_per_sec_mac
+                amount_mac -= (180 * (1 - rate))
             else:    # 뽑아야 하는 양보다 남은 양이 적을 때 우선 남은 양만 뽑기
                 sec = (amount_mac / amount_per_sec_mac) + 0.5
                 mac_2nd = (180 * (1 - rate)) - amount_mac
                 isReplay2 = 1
+                amount_mac = 0
 
         # pump output
         GPIO.output(13, True)
@@ -50,5 +53,7 @@ def pumpMac(rate, isFirst2, isReplay2, amount_mac, mac_2nd, amount_per_sec_mac):
 
         isReplay2 = 0
         isFirst2 = 1
+        
+        amount_mac -= (sec - 1.3) * amount_per_sec_mac
 
         return isFirst2, isReplay2, amount_mac, mac_2nd
